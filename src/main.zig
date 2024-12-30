399ede42e7b9c039be136b6484b0f4c0dc83babe
Devante Coley <devantecoley802@gmail.com>
2024-12-29 22:23:36 -0500

    TODO >> Complete the loading of the rom in the main function! Added a whole lot of stuff to main including argument parsing and file opening!

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,45 @@ const rl = @import("raylib");
 
 pub fn main() !void {
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
     var nes: components.Nes = .{ .Cpu = .{}, .Ppu = .{}, .Bus = .{} };
     nes.init();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    defer _ = gpa.deinit();
+
+    var allocator = gpa.allocator();
+
+    var args = std.process.args();
+    var path: ?[]u8 = null;
+
+    while (true) {
+        const argument = args.next();
+        if (argument == null) {
+            break;
+        }
+        if (std.mem.eql(u8, argument.?, "-p")) {
+            const path_arg = args.next();
+            path = @constCast(path_arg.?[0..path_arg.?.len]);
+        }
+    }
+
+    if (path == null) {
+        std.debug.print("No Path variable found!\n", .{});
+        return;
+    }
+
+    std.debug.print("Path: {s}\n", .{path.?});
+
+    const working_directory = std.fs.cwd();
+
+    const ines_file = try working_directory.openFile(path.?, .{});
+    defer ines_file.close();
+
+    const rom = try ines_file.readToEndAlloc(allocator, 768000);
+    defer allocator.free(rom);
+
+    std.debug.print("Rom Loaded: {d}!\n\n", .{rom.len});
 }
 
 test "simple test" {
