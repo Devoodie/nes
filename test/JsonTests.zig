8ffa5a628baa2519cfa45901b7c570c48b78333b
Devante Coley <devantecoley802@gmail.com>
2025-02-05 15:39:30 -0500

    TODO >> Fix BRK instruction! Added All unused instructions so they are skipped during the test!

--- test/JsonTests.zig
+++ test/JsonTests.zig
@@ -3,8 +3,18 @@ const test_structs = @import("json");
 const components = @import("nes");
 
 test "JSON 6502 Tests" {
+    const name_slices = []const u8;
+    const unused_instructions = [_]name_slices{
+        "03.json", "07.json", "0b.json", "0f.json", "13.json", "17.json", "1b.json", "1f.json",
+        "23.json", "27.json", "2b.json", "2f.json", "33.json", "37.json", "3b.json", "3f.json",
+        "43.json", "47.json", "4b.json", "4f.json", "53.json", "57.json", "5b.json", "5f.json",
+        "63.json", "67.json", "6b.json", "6f.json", "73.json", "77.json", "7b.json", "7f.json",
+        "83.json", "87.json", "8b.json", "8f.json", "93.json", "97.json", "9b.json", "9f.json",
+        "a3.json", "a7.json", "ab.json", "af.json", "b3.json", "b7.json", "bb.json", "bf.json",
+        "c3.json", "c7.json", "cb.json", "cf.json", "d3.json", "d7.json", "db.json", "df.json",
+        "e3.json", "e7.json", "eb.json", "ef.json", "f3.json", "f7.json", "fb.json", "ff.json",
+    };
 
-    const unused_instructions: [8]u8 = .{"03.zig", "07.zig", "0b.zig", "0f.zig", "13.zig", "17.zig", "1b.zig", "1f.zig"};
     var nes: components.Nes = .{ .Cpu = .{}, .Ppu = .{}, .Bus = .{} };
 
     nes.init();
@@ -33,11 +43,15 @@ test "JSON 6502 Tests" {
     var json_string: []u8 = undefined;
     var iterations: u16 = 0;
 
-    while (try iterator.next()) |kind| {
+    outer: while (try iterator.next()) |kind| {
         //get the json
         iterations += 1;
         std.debug.print("Iterations: {d}\n", .{iterations});
         const filename = kind.name;
+
+        for (unused_instructions) |name| {
+            if (std.mem.eql(u8, filename, name)) continue :outer;
+        }
 
         var test_file = try test_dir.openFile(filename, .{});
         try test_file.seekTo(0);
